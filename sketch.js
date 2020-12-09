82dce116559bae8185221f7be917c64527e8adbd
NishantKC <71525407+NishantKC@users.noreply.github.com>
2020-12-09 15:53:52 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function preload(){
+
+}
+function setup(){
+  createCanvas(1510,500)
+  wkey1 = createSprite(75,250,100,400)
+  wkey1.shapeColor = "white"
+  wkey2 = createSprite(180,250,100,400)
+  wkey2.shapeColor = "white"
+  wkey3 = createSprite(285,250,100,400)
+  wkey3.shapeColor = "white"
+  wkey4 = createSprite(390,250,100,400)
+  wkey4.shapeColor = "white"
+  wkey5 = createSprite(495,250,100,400)
+  wkey5.shapeColor = "white"
+  wkey6 = createSprite(600,250,100,400)
+  wkey6.shapeColor = "white"
+  wkey7 = createSprite(705,250,100,400)
+  wkey7.shapeColor = "white"
+  bkey1 = createSprite(127,185,50,270)
+  bkey1.shapeColor = "black"
+  bkey2 = createSprite(232,185,50,270)
+  bkey2.shapeColor = "black"
+  bkey3 = createSprite(652,185,50,270)
+  bkey3.shapeColor = "black"
+  bkey4 = createSprite(442,185,50,270)
+  bkey4.shapeColor = "black"
+  bkey5 = createSprite(547,185,50,270)
+  bkey5.shapeColor = "black"
+
+
+  wkey8 = createSprite(810,250,100,400)
+  wkey8.shapeColor = "white"
+  wkey9 = createSprite(915,250,100,400)
+  wkey9.shapeColor = "white"
+  wkey10 = createSprite(1020,250,100,400)
+  wkey10.shapeColor = "white"
+  wkey11 = createSprite(1125,250,100,400)
+  wkey11.shapeColor = "white"
+  wkey12 = createSprite(1230,250,100,400)
+  wkey12.shapeColor = "white"
+  wkey13 = createSprite(1335,250,100,400)
+  wkey13.shapeColor = "white"
+  wkey14 = createSprite(1440,250,100,400)
+  wkey14.shapeColor = "white"
+  bkey6 = createSprite(862,185,50,270)
+  bkey6.shapeColor = "black"
+  bkey7 = createSprite(967,185,50,270)
+  bkey7.shapeColor = "black"
+  bkey8 = createSprite(1177,185,50,270)
+  bkey8.shapeColor = "black"
+  bkey9 = createSprite(1282,185,50,270)
+  bkey9.shapeColor = "black"
+  bkey10 = createSprite(1387,185,50,270)
+  bkey10.shapeColor = "black"
+}
+function draw(){
+background("red")
+drawSprites()
+}
